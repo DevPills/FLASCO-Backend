5dfdad029f987c60237a89dc8b5d3c18210d53f5
JoseGet <getuliojose96@gmail.com>
2025-05-17 18:05:34 -0400

    Commit commitado

--- flasco/repositories/video_repository.py
+++ flasco/repositories/video_repository.py
@@ -15,3 +15,8 @@ class VideoRepository:
         await self.db_session.refresh(item)
 
         return item
+    
+    async def delete(self, item: T) -> T:
+        self.db_session.delete(item)
+        await self.db_session.commit()
+        await self.db_session.refresh(item)
--- flasco/usecases/video_delete_usecase.py
+++ flasco/usecases/video_delete_usecase.py
@@ -1,13 +1,19 @@
+from datetime import time
 from flasco.database.filestorage import SupabaseStorage
+from flasco.repositories.video_repository import VideoRepository
 
 
 class DeleteVideoUseCase: 
-    def __init__(self, supabase_service: SupabaseStorage):
+    def __init__(self, supabase_service: SupabaseStorage, video_respository: VideoRepository):
         self.video_service = supabase_service
+        self.video_respository = video_respository
     
     async def execute(self, video_name, paths):
-        response = await self.video_service.remove(
+        deleted_video = await self.video_service.remove(
             file_name=video_name,
             paths=paths
         )
+        
+        response = await self.video_respository.delete(item=deleted_video)
+
         return response
