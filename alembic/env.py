6f6c09731a71b5ef0e8b4e85bac59d2a38fd558d
Samuel Monteiro <gomessamuel.14@gmail.com>
2025-06-10 20:33:45 -0400

    teste banco

--- alembic/env.py
+++ alembic/env.py
@@ -45,16 +45,19 @@ def do_run_migrations(connection):
 
 
 async def run_migrations_online() -> None:
-    """Run migrations in 'online' mode with asyncpg."""
+    """Run migrations in 'online' mode with asyncpg and SSL."""
     connectable = create_async_engine(
         settings.DATABASE_URL,
         poolclass=pool.NullPool,
+        connect_args={"sslmode": "require"},
+        echo=True,
     )
 
     async with connectable.connect() as connection:
         await connection.run_sync(do_run_migrations)
 
     await connectable.dispose()
+
 
 
 if context.is_offline_mode():
